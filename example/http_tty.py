484405780622c3a9177f1d740d684dd0dfd8167f
snakeztc <China510@>
2021-02-25 18:32:40 +0800

    add http examples

--- example/http_tty.py
+++ example/http_tty.py
@@ -1,7 +1,9 @@
-from r2base.engine.ranker import Ranker
+import requests
+import os
+
+host_url = 'http://localhost:8000'
 
 if __name__ == "__main__":
-    ranker = Ranker()
     index = 'wiki-basic'
 
     while True:
@@ -13,10 +15,13 @@ if __name__ == "__main__":
             q = query
             filter = None
 
-        res = ranker.query(index, {'match': {'text': q},
-                                   'filter': filter,
-                                   'size': 5})
-        print('{} results'.format(len(res)))
+        body = {'match': {'text': q},
+                'filter': filter,
+                'size': 5}
+        res = requests.post(os.path.join(host_url, 'r2base/v1/search/wiki-basic/query'), json={'query': body})
+        res = res.json()
+        print('{} results'.format(len(res['ranks'])))
+        res = res['ranks']
         if len(res) > 0:
             print(res[0]['score'], res[0]['_source']['title'], res[0]['_source'])
         else:
